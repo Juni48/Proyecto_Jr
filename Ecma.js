d1f69c4a4e51d977f79eaffc256b6fef457f8418
Juan Picos <juanpicos98@outlook.es>
2022-07-14 01:01:49 -0500

    Se agrego el DOM a input y boton para agregar nuevas pinturas

--- Ecma.js
+++ Ecma.js
@@ -1,12 +1,5 @@
+
 console.log("Hola desde la consola");
-
-
-
-
-
-
-
-
 
 const isCompleted = false;
 const tasks = [];
@@ -36,8 +29,91 @@ function deleteTask(tasks, index) {
     return newTasks;
   }
 
+function captura(){
+  const nombrePint = document.getElementById('inputPintura').value;
+  addTask(nombrePint);
+}
 
-addTask("Monalisa", true)
+
+addTask("Monalisa")
 addTask("Noche estrellada")
 
 console.log(tasks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const container = document.getElementById('app');
+// container.className = 'recomendaciones';
+
+// const title = createNode('h1', 'Nueva Pintura');
+// title.className = "h1P"
+
+// const addButton = createNode('button', 'Add', { type: 'button'});
+// addButton.className = "buttonP"
+
+// const input = createNode('input', '', { type: 'text', placeholder: 'New task...'});
+// input.className = "inputPintura"
+
+
+// appendChildren(container, [title, input, addButton]);
+
+// function createNode(type, child, attrs){
+//     let node = document.createElement(type);
+//     if(attrs){
+//         Object.keys(attrs).map(function(attr){
+//             node[attr] = attr[attr];
+//         });
+//     }
+//     if (typeof child === "string"){
+//         node.appendChild(document.createTextNode(child));
+//     } else {
+//         node.appendChild(child);
+//     }
+//     return node
+// }
+
+// function appendChildren(node, children){
+//     const documentFragment = document.createDocumentFragment();
+//     children.forEach(function(child){
+//         documentFragment.appendChild(child);
+//     })
+//     node.appendChild(documentFragment);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
